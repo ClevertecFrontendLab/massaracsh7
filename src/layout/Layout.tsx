70e47417a304ebdd77972dc495673c95621e018e
massaracsh7 <fatum7@gmail.com>
2025-04-02 18:58:28 +0300

    style(layout): add layout structure style

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -6,11 +6,19 @@ import NavigationMenu from '~/components/NavigationMenu/NavigationMenu';
 import Sidebar from '~/components/Sidebar/Sidebar';
 
 const Layout = ({ children }: { children: React.ReactNode }) => (
-    <Box>
+    <Box display='flex' flexDirection='column' height='100vh'>
         <Header />
-        <NavigationMenu />
-        <Sidebar />
-        <Content>{children}</Content>
+        <Box display='flex' flex='1'>
+            <Box width='256px' shadow='base'>
+                <NavigationMenu />
+            </Box>
+            <Box flex='1' pt={8} py={6}>
+                <Content>{children}</Content>
+            </Box>
+            <Box width='208px' bg='gray.100'>
+                <Sidebar />
+            </Box>
+        </Box>
     </Box>
 );
 
