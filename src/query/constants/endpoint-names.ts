02e9ad99e7303015f608f8b0f179b200cf33cb56
massaracsh7 <fatum7@gmail.com>
2025-04-29 22:19:51 +0300

    feat(query): add api constants

--- src/query/constants/endpoint-names.ts
+++ src/query/constants/endpoint-names.ts
@@ -1,3 +1,7 @@
 export enum EndpointNames {
     GET_POSTS = 'getPosts',
+    GET_RECIPES = 'getRecipes',
+    GET_RECIPE_BY_ID = 'getRecipeById',
+    GET_CATEGORIES = 'getCategories',
+    GET_CATEGORY_BY_ID = 'getCategoryById',
 }
