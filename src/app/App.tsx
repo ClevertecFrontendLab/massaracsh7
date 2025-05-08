91df4f69a413e5149c5558500e21cbb3aa9657f2
massaracsh7 <fatum7@gmail.com>
2025-05-08 21:34:47 +0300

    refactor(loader): remove loader to app

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,15 @@
 import { BrowserRouter as Router } from 'react-router';
 
+import FullLoader from '~/components/FullLoader/FullLoader';
+import { useAppSelector } from '~/store/hooks';
+
 import AppRoutes from './AppRoutes';
 
 function App() {
+    const isLoading = useAppSelector((state) => state.app.isLoading);
     return (
         <Router>
+            {isLoading && <FullLoader />}
             <AppRoutes />
         </Router>
     );
